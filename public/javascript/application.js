d3c4d818c11c36b80403f99823afcdd450cc7bf1
Brett Weinkauf <bweinkau@ualberta.ca>
2016-04-21 19:13:43 +0000

    CSS for home page

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,36 +2,26 @@
 var player;
 function onYouTubePlayerAPIReady() {
 
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'MS91knuzoOA',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
+ player = new YT.Player('player', {
+   height: '390',
+   width: '640',
+   videoId: 'MS91knuzoOA',
+   events: {
+     'onReady': onPlayerReady,
+     'onStateChange': onPlayerStateChange
+   }
+ });
 }
 // autoplay video
 function onPlayerReady(event) {
-    event.target.playVideo();
+   event.target.playVideo();
 }
 
 // when video ends
 function onPlayerStateChange(event) {
-  if(event.data === 0) {
-	
-		player = new YT.Player('player', {
-			height: '390',
-			width: '640',
-			videoId: 'CxKWTzr-k6s',
-			events: {
-				'onReady': onPlayerReady,
-				'onStateChange': onPlayerStateChange
-			}
-		});
-
-  }
+ if(event.data === 0) {
+ player.loadVideoById('CxKWTzr-k6s');
+ }
 }
 
 $(document).ready(function() {
