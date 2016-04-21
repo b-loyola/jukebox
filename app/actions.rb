15459c69ce1c0f15cce95c2f17ed55b3fa67c459
b-silva <berna.loyola@gmail.com>
2016-04-21 02:52:43 +0000

    added basic room page

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,25 @@
 # Homepage (Root path)
+
+
 get '/' do
-	@rooms = Room.all
+	redirect '/rooms'
+end
+
+get '/rooms/?' do
+	@room = Room.new
   erb :index
+end
+
+post '/rooms/?' do
+	@room = Room.new(name: params[:room_name])
+	if @room.save
+		@room.reload
+		redirect "/rooms/#{@room.id}"
+	else
+		erb :index
+	end
+end
+
+get '/rooms/:id/?' do
+	erb :'rooms/index'
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,39 @@
+// create youtube player
+var player;
+function onYouTubePlayerAPIReady() {
+
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'MS91knuzoOA',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+// autoplay video
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// when video ends
+function onPlayerStateChange(event) {
+  if(event.data === 0) {
+	
+		player = new YT.Player('player', {
+			height: '390',
+			width: '640',
+			videoId: 'CxKWTzr-k6s',
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+
+  }
+}
+
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
